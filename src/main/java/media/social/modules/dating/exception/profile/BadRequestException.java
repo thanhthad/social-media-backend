c3f8db0745a43d02782c3c417aa9efa0cdcaf859
thanhthad <tauhaitac3k@gmail.com>
2026-08-07 18:58:43 +0700

    feat(dating): implement dating profile service and cache

---
+++ src/main/java/media/social/modules/dating/exception/profile/BadRequestException.java
@@ -0,0 +1,7 @@
+package media.social.modules.dating.exception.profile;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
--- src/main/java/media/social/modules/user/service/cache/UserProfileCacheService.java
+++ src/main/java/media/social/modules/user/service/cache/UserProfileCacheService.java
@@ -1,6 +1,5 @@
 package media.social.modules.user.service.cache;
 
-
 import media.social.modules.user.dto.response.cache.PublicUserProfileCacheResponse;
 import media.social.modules.user.dto.response.cache.UserFollowStatCacheResponse;
 
